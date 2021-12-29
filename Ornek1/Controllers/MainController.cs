d452c7941ea229f439e7460a1844d193298ed6ee
e-mod <e-mod@DESKTOP-E9LRMFA>
2021-12-29 14:43:01 +0300

    Add project files.

---
+++ Ornek1/Controllers/MainController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ornek1.Controllers
+{
+    public class MainController : ApiController
+    {
+        Dictionary<string, string> verilerim = new Dictionary<string, string>() {{"ali","veli"},{"ayse","fatma"},{"ahmet","mehmet"} 
+        };
+        
+        //localhost:Portno/api/Main?ad=ayse
+        //localhost:Portno/api/Main?ad=ahmet
+
+        [HttpGet]
+        public HttpResponseMessage Getir(string ad) 
+        {
+            string sonuc;
+            verilerim.TryGetValue(ad, out sonuc);
+
+            if (sonuc==null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Aradığınız veri bulunamadı!");
+            }
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, sonuc);
+        }
+    }
+}
---
+++ Ornek1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ornek1.MvcApplication" Language="C#" %>
